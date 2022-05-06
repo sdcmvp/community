078da83d5bb63dc65e530b72c570cef61104795f
13635496126 <2363976330@qq.com>
2022-05-06 09:45:16 +0800

    第三次提交

---
+++ src/test/java/com/nowcoder/community/SensitiveTest.java
@@ -0,0 +1,25 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.SensitiveFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTest {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "这里不可以赌?博，不能吸毒，可以开票";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+}
